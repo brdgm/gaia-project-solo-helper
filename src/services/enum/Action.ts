ddd3a327f3fc79a9171ef22016fc7420afee0b48
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-05-06 22:21:26 +0200

    actions

--- src/services/enum/Action.ts
+++ src/services/enum/Action.ts
@@ -2,11 +2,16 @@
  * Action
  */
 enum Action {
+  ADVANCE_HIGHEST_RESEARCH_AREA = 'advance-highest-research-area',
+  ADVANCE_RANDOM_RESEARCH_AREA = 'advance-random-research-area',
+  BUILD_MINE = 'build-mine',
   FACTION_ACTION = 'faction-action',
+  POWER_QIC_ACTION = 'power-qic-action',
+  UPGRADE = 'upgrade',
+
   BLOCK_POWER_ACTION = 'block-power-action',
   GAIN_VICTORY_POINTS = 'gain-victory-points',
   ADVANCE_CULT_TRACK = 'advance-cult-track',
-  UPGRADE = 'upgrade',
   TAKE_FAVOR_TILE = 'take-favor-tile',
   TRANSFORM_AND_BUILD = 'transform-and-build',
   TRADE = 'trade',
