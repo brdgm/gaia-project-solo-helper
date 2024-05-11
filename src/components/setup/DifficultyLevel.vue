c393d70ec22efcf817ecc7710833602cb78e6929
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-05-11 08:30:12 +0200

    improve responsive behavior

--- src/components/setup/DifficultyLevel.vue
+++ src/components/setup/DifficultyLevel.vue
@@ -2,14 +2,14 @@
   <h3 class="mt-4 mb-3">{{t('setup.difficultyLevel.title')}}</h3>
 
   <div class="row">
-    <div class="col-1 text-end">
+    <div class="col-3 col-sm-2 col-md-1 text-end">
       <label for="difficultyLevel" class="form-label">{{t('setup.difficultyLevel.easy')}}</label>
     </div>
-    <div class="col-8 col-md-4">
+    <div class="col-6 col-sm-8 col-md-5">
       <input type="range" class="form-range" min="1" max="5" id="difficultyLevel"
           :value="difficultyLevel" @input="updateDifficultyLevel($event)">
     </div>
-    <div class="col-1">
+    <div class="col-3 col-sm-2 col-md-1">
       <label for="difficultyLevel" class="form-label">{{t('setup.difficultyLevel.hard')}}</label>
     </div>
   </div>  
