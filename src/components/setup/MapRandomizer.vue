8ace3a9336a928f6df320f6a36ae68e455465bd1
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-11-26 21:18:54 +0100

    map generator toggle button

---
+++ src/components/setup/MapRandomizer.vue
@@ -0,0 +1,30 @@
+<template>
+  <p>Map Randomizer...</p>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { useI18n } from 'vue-i18n'
+import AppIcon from '../structure/AppIcon.vue'
+import { useStateStore } from '@/store/state'
+
+export default defineComponent({
+  name: 'MapRandomizer',
+  components: {
+    AppIcon
+  },
+  setup() {
+    const { t } = useI18n()
+    const state = useStateStore()
+
+    return { t, state }
+  },
+  computed: {
+  },
+  methods: {
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
