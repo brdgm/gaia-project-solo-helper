653ae14543785dcc531197daa8aa5252f5e59734
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-11-29 18:26:56 +0100

    lost fleet map positioning

--- src/components/setup/MapRandomizer.vue
+++ src/components/setup/MapRandomizer.vue
@@ -24,6 +24,17 @@
               <use x="68" y="55" xlink:href="#spaceSectorArrow"/>
             </svg>
           </div>
+          <div>
+            <div v-for="(deepSpaceSector,index) of deepSpaceSectors" :key="deepSpaceSector.id" class="deepSpaceSector" @click="deepSpaceSector.randomizeRotationFlip()"
+                :style="deepSpaceSectorTransform(deepSpaceSector, index)">
+              <AppIcon type="map-deep-space-sector" :name="`${deepSpaceSector.id + (deepSpaceSector.outline ? '-outline' : '')}`" extension="webp"/>
+            </div>
+          </div>
+          <div>
+            <div v-for="(interspace,index) of interspaces" :key="index" class="interspace">
+              <AppIcon type="map-interspace" :name="interspace" extension="webp"/>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -43,6 +54,8 @@ import { useStateStore } from '@/store/state'
 import MapGenerator from '@/services/map/MapGenerator'
 import SpaceSector from '@/services/map/SpaceSector'
 import Expansion from '@/services/enum/Expansion'
+import DeepSpaceSector from '@/services/map/DeepSpaceSector'
+import Interspace from '@/services/map/Interspace'
 
 export default defineComponent({
   name: 'MapRandomizer',
@@ -63,6 +76,12 @@ export default defineComponent({
     spaceSectors() : readonly SpaceSector[] {
       return this.mapGenerator.spaceSectors
     },
+    deepSpaceSectors() : readonly DeepSpaceSector[] {
+      return this.mapGenerator.deepSpaceSectors
+    },
+    interspaces() : readonly Interspace[] {
+      return this.mapGenerator.interspaces
+    },
     isValid() : boolean {
       return this.mapGenerator.isValid()
     },
@@ -71,6 +90,15 @@ export default defineComponent({
     }
   },
   methods: {
+    deepSpaceSectorTransform(sector: DeepSpaceSector, index: number) {
+      let rotation = sector.rotation * 120
+      if (index == 2 || index == 4 || index == 5 || index == 7) {
+        rotation += 60
+      }
+      const originX = 43.5
+      const originY = 38
+      return `transform-origin:${originX}px ${originY}px;transform:rotate(${rotation}deg);`
+    }
   }
 })
 </script>
@@ -84,11 +112,11 @@ export default defineComponent({
 .map {
   .spaceSector {
     position: absolute;
-    display: inline-block;
     width: 150px;
     filter: drop-shadow(0 0 0.25rem #fff);
     cursor: pointer;
     user-select: none;
+    z-index: 30;
     img {
       width: 100%;
     }
@@ -111,6 +139,25 @@ export default defineComponent({
           font-weight: 800;
         }
       }
+    }
+  }
+  .deepSpaceSector {
+    position: absolute;
+    width: 76px;
+    cursor: pointer;
+    user-select: none;
+    z-index: 20;
+    img {
+      width: 100%;
+    }
+  }
+  .interspace {
+    position: absolute;
+    width: 46px;
+    user-select: none;
+    z-index: 20;
+    img {
+      width: 100%;
     }
   }
   &.baseGame.fourPlayer {
@@ -236,6 +283,80 @@ export default defineComponent({
       top: calc(244px + 0px);
       left: calc(112px + 140px + 140px);
     }
+    /* deep space sectors */
+    .deepSpaceSector:nth-child(1) {
+      top: 60px;
+      left: 107px;
+    }
+    .deepSpaceSector:nth-child(2) {
+      top: calc(60px - 48px);
+      left: calc(107px + 140px);
+    }
+    .deepSpaceSector:nth-child(3) {
+      top: 45px;
+      left: 432px;
+    }
+    .deepSpaceSector:nth-child(4) {
+      top: 222px;
+      left: 498px;
+    }
+    .deepSpaceSector:nth-child(5) {
+      top: 368px;
+      left: 377px;
+    }
+    .deepSpaceSector:nth-child(6) {
+      top: calc(368px + 48px);
+      left: calc(377px - 140px);
+    }
+    .deepSpaceSector:nth-child(7) {
+      top: 383px;
+      left: 50px;
+    }
+    .deepSpaceSector:nth-child(8) {
+      top: 204px;
+      left: -15px;
+    }
+    /* interspaces */
+    .interspace:nth-child(1) {
+      top: 175px;
+      left: 164px;
+    }
+    .interspace:nth-child(2) {
+      top: 191px;
+      left: 248px;
+    }
+    .interspace:nth-child(3) {
+      top: 126px;
+      left: 304px;
+    }
+    .interspace:nth-child(4) {
+      top: 143px;
+      left: 388px;
+    }
+    .interspace:nth-child(5) {
+      top: 224px;
+      left: 416px;
+    }
+    .interspace:nth-child(6) {
+      top: 289px;
+      left: 360px;
+    }
+    .interspace:nth-child(7) {
+      top: 272px;
+      left: 276px;
+    }
+    .interspace:nth-child(8) {
+      top: 337px;
+      left: 220px;
+    }
+    .interspace:nth-child(9) {
+      top: 321px;
+      left: 136px;
+    }
+    .interspace:nth-child(10) {
+      top: 239px;
+      left: 108px;
+    }
   }
   &.lostFleet.threePlayer {
     position: relative;
@@ -278,13 +399,79 @@ export default defineComponent({
       top: calc(244px + 0px);
       left: calc(112px + 140px + 140px);
     }
+    /* deep space sectors */
+    .deepSpaceSector:nth-child(1) {
+      top: 60px;
+      left: 107px;
+    }
+    .deepSpaceSector:nth-child(2) {
+      top: calc(60px - 48px);
+      left: calc(107px + 140px);
+    }
+    .deepSpaceSector:nth-child(3) {
+      top: 190px;
+      left: 404px;
+    }
+    .deepSpaceSector:nth-child(4) {
+      top: 124px;
+      left: 386px;
+    }
+    .deepSpaceSector:nth-child(5) {
+      top: 368px;
+      left: 377px;
+    }
+    .deepSpaceSector:nth-child(6) {
+      top: calc(368px + 48px);
+      left: calc(377px - 140px);
+    }
+    .deepSpaceSector:nth-child(7) {
+      top: 383px;
+      left: 50px;
+    }
+    .deepSpaceSector:nth-child(8) {
+      top: 204px;
+      left: -15px;
+    }
+    /* interspaces */
+    .interspace:nth-child(1) {
+      top: 175px;
+      left: 164px;
+    }
+    .interspace:nth-child(2) {
+      top: 191px;
+      left: 248px;
+    }
+    .interspace:nth-child(3) {
+      top: 126px;
+      left: 304px;
+    }
+    .interspace:nth-child(4) {
+      top: 289px;
+      left: 360px;
+    }
+    .interspace:nth-child(5) {
+      top: 272px;
+      left: 276px;
+    }
+    .interspace:nth-child(6) {
+      top: 337px;
+      left: 220px;
+    }
+    .interspace:nth-child(7) {
+      top: 321px;
+      left: 136px;
+    }
+    .interspace:nth-child(8) {
+      top: 239px;
+      left: 108px;
+    }
   }
   &.lostFleet.twoPlayer {
     position: relative;
     height: 504px;
     width: 570px;
     transform: translate(14px,-22px) rotate(30deg);
-    margin-bottom: -42px;
+    margin-bottom: -70px;
     .spaceSector:nth-child(1) {
       top: 96px;
       left: 28px;
@@ -312,6 +499,56 @@ export default defineComponent({
     .spaceSector:nth-child(7) {
       top: calc(244px + 48px);
       left: calc(112px + 140px);
+    }
+    /* deep space sectors */
+    .deepSpaceSector:nth-child(1) {
+      top: 60px;
+      left: 107px;
+    }
+    .deepSpaceSector:nth-child(2) {
+      top: 271px;
+      left: 358px;
+    }
+    .deepSpaceSector:nth-child(3) {
+      top: 92px;
+      left: 292.5px;
+    }
+    .deepSpaceSector:nth-child(4) {
+      top: 383px;
+      left: 50px;
+    }
+    .deepSpaceSector:nth-child(5) {
+      top: 204px;
+      left: -15px;
+    }
+    .deepSpaceSector:nth-child(6) {
+      top: calc(368px + 48px);
+      left: calc(377px - 140px);
+    }
+    /* interspaces */
+    .interspace:nth-child(1) {
+      top: 175px;
+      left: 164px;
+    }
+    .interspace:nth-child(2) {
+      top: 191px;
+      left: 248px;
+    }
+    .interspace:nth-child(3) {
+      top: 272px;
+      left: 276px;
+    }
+    .interspace:nth-child(4) {
+      top: 337px;
+      left: 220px;
+    }
+    .interspace:nth-child(5) {
+      top: 321px;
+      left: 136px;
+    }
+    .interspace:nth-child(6) {
+      top: 239px;
+      left: 108px;
     }
   }
 }
